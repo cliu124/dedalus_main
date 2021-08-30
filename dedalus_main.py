7778c5900aff1e47745f6ed2e21fa190a734028b
cliu124 <39631690+cliu124@users.noreply.github.com>
2021-08-29 23:00:20 -0700

    Update 2021/08/28

--- dedalus_main.py
+++ dedalus_main.py
@@ -25,101 +25,40 @@ flag.path='./'
 flag.print_screen()
 flag.current_path='/projects/chli3324/dedalus/'
 flag.flow='IFSC_2D_without_shear'
-
-#flag.print_file()
-#print(', '.join("%s: %s, \n" % item for item in flag_attrs.items()))
-
-#Ra_ratio=1.1
+flag.A_elevator=1
+flag.A_noise=0
+flag.A_shear=1
 tau=0.01
 Pr=7
 
 R_rho=1/tau/flag.Ra_ratio
-
 k_opt=(1/2*(-2-flag.Ra_ratio+np.sqrt(flag.Ra_ratio**2+8*flag.Ra_ratio)))**(1/4)
-logger.info('The optimal horizontal wavenumber is %f for Rayleigh ratio %f' %(k_opt,flag.Ra_ratio))
 
 Lx2d = 32
 Lz2d = 32
 grid_l_opt=8
-Lx, Lz = (Lx2d*2*np.pi/k_opt, Lz2d*2*np.pi/k_opt)
-nx, nz = (grid_l_opt*Lx2d,grid_l_opt*Lz2d)
+flag.Lx, flag.Lz = (Lx2d*2*np.pi/k_opt, Lz2d*2*np.pi/k_opt)
+flag.Nx, flag.Nz = (grid_l_opt*Lx2d,grid_l_opt*Lz2d)
 
-ks=2*np.pi/Lz
+flag.ks=2*np.pi/flag.Lz
 
 #Ri=1/1000*Pr*(1-1/R_rho)/tau**2/ks**2
 Ri=1
-logger.info('The ks=%f, Ri=%f ' %(ks, Ri))
-# Create bases and domain
-#x_basis = de.Fourier('x', nx, interval=(0, Lx), dealias=3/2)
-#z_basis = de.Fourier('z',nz, interval=(0,Lz), dealias=3/2)
-#domain = de.Domain([x_basis,  z_basis], grid_dtype=np.float64)
-
-#problem = de.IVP(domain, variables=['p','u','w','S','T'])
-#problem.parameters['Ra_ratio']=flag.Ra_ratio
-#problem.parameters['pi']=np.pi
-#problem.parameters['R_rho']=1/flag.Ra_ratio/tau
-#problem.parameters['Pr']=Pr
-#problem.parameters['tau']=tau
-#problem.parameters['ks']=ks
-#F_sin=ks/tau*np.sqrt(Pr*(1-1/R_rho)/Ri)
-#logger.info('The shear forcing amplitude is %f' %(F_sin))
-
-#problem.parameters['F_sin']=F_sin
-#problem=dedalus_setup.governing_equation(problem,'IFSC_2D_with_shear')
-problem, domain=dedalus_setup.governing_equation(flag)
-F_sin=1
-Ra_ratio=1
-##These two equations are dealing with the singularity of the following two equations when kx=kx=0
-#problem.add_equation("p=0",condition="(nx==0) and (nz==0)")
-#problem.add_equation("u=0",condition="(nx==0) and (nz==0)")
-
-#problem.add_equation("dx(u) + dz(w) = 0",condition="(nx!=0) or (nz!=0)") ##divergence free constraint is singular at kx=kz=0
-#problem.add_equation("dt(S) - (dx(dx(S)) + dz(dz(S)))  + w                  = - u*dx(S) - w*dz(S)")
-
-##with shear
-#problem.parameters['Lz']=Lz
-#problem.parameters['pi']=np.pi
-#problem.add_equation(" -( dx(dx(u)) + dz(dz(u)) ) + dx(p) =F_sin*sin(ks*z)",condition="(nx!=0) or (nz!=0)")
-
-
-##without shear
-#problem.add_equation(" -( dx(dx(u)) + dz(dz(u)) ) + dx(p) =0",condition="(nx!=0) or (nz!=0)")
-#This is also singular at kx=kz=0, this is unique for the IFSC, because the momentum is reduced as the algebraic constraint
-#problem.add_equation(" -( dx(dx(w)) + dz(dz(w)) ) + dz(p) -(T-S*Ra_ratio)   =0")
-#problem.add_equation(" -( dx(dx(T)) + dz(dz(T)) ) + w                       =0")
-
+domain=flag.build_domain()
+problem=flag.governing_equation(domain)
 
 ts = de.timesteppers.RK443
 
 solver =  problem.build_solver(ts)
-x = domain.grid(0)
-z = domain.grid(1)
-u = solver.state['u']
-w = solver.state['w']
-S = solver.state['S']
-p = solver.state['p']
-T = solver.state['T']
 
-gshape = domain.dist.grid_layout.global_shape(scales=1)
-slices = domain.dist.grid_layout.slices(scales=1)
-rand = np.random.RandomState(seed=23)
-noise = rand.standard_normal(gshape)[slices]
-
-A_elevator=1
-A_noise=0
-A_shear=1
-w['g'] = A_elevator*np.sin(k_opt*x) + A_noise*noise
-u['g'] = A_noise*noise + A_shear*F_sin/ks**2*np.sin(ks*z)
-S['g'] = -1/Ra_ratio*(k_opt**2+1/k_opt**2)*A_elevator*np.sin(k_opt*x) + A_noise*noise
-T['g'] = -1/(k_opt**2)*A_elevator*np.sin(k_opt*x) + A_noise*noise
-p['g'] = A_noise*noise
+flag.initial_condition(domain,solver)
 
 solver.stop_sim_time = 1
 solver.stop_wall_time = np.inf
 solver.stop_iteration = np.inf
 
 #initial_dt = 0.02*Lx/nx
-initial_dt=0.2*Lx/nx/(F_sin/ks**2)
+initial_dt=0.2*flag.Lx/flag.Nx/(flag.F_sin/flag.ks**2)
 cfl = flow_tools.CFL(solver,initial_dt,safety=0.8,max_change=1,cadence=8)
 cfl.add_velocities(('u','w'))
 
@@ -150,6 +89,53 @@ logger.info('Run time: %f cpu-hr' %((end_time-start_time)/60/60*domain.dist.comm
 #merge process data
 post.merge_process_files(flag.name,cleanup=True)
 flag.print_file()
+
+
+# Create bases and domain
+#x_basis = de.Fourier('x', nx, interval=(0, Lx), dealias=3/2)
+#z_basis = de.Fourier('z',nz, interval=(0,Lz), dealias=3/2)
+#domain = de.Domain([x_basis,  z_basis], grid_dtype=np.float64)
+
+#problem = de.IVP(domain, variables=['p','u','w','S','T'])
+#problem.parameters['Ra_ratio']=flag.Ra_ratio
+#problem.parameters['pi']=np.pi
+#problem.parameters['R_rho']=1/flag.Ra_ratio/tau
+#problem.parameters['Pr']=Pr
+#problem.parameters['tau']=tau
+#problem.parameters['ks']=ks
+#F_sin=ks/tau*np.sqrt(Pr*(1-1/R_rho)/Ri)
+#logger.info('The shear forcing amplitude is %f' %(F_sin))
+
+#problem.parameters['F_sin']=F_sin
+#problem=dedalus_setup.governing_equation(problem,'IFSC_2D_with_shear')
+
+#F_sin=1
+#Ra_ratio=1
+##These two equations are dealing with the singularity of the following two equations when kx=kx=0
+#problem.add_equation("p=0",condition="(nx==0) and (nz==0)")
+#problem.add_equation("u=0",condition="(nx==0) and (nz==0)")
+
+#problem.add_equation("dx(u) + dz(w) = 0",condition="(nx!=0) or (nz!=0)") ##divergence free constraint is singular at kx=kz=0
+#problem.add_equation("dt(S) - (dx(dx(S)) + dz(dz(S)))  + w                  = - u*dx(S) - w*dz(S)")
+
+##with shear
+#problem.parameters['Lz']=Lz
+#problem.parameters['pi']=np.pi
+#problem.add_equation(" -( dx(dx(u)) + dz(dz(u)) ) + dx(p) =F_sin*sin(ks*z)",condition="(nx!=0) or (nz!=0)")
+
+
+##without shear
+#problem.add_equation(" -( dx(dx(u)) + dz(dz(u)) ) + dx(p) =0",condition="(nx!=0) or (nz!=0)")
+#This is also singular at kx=kz=0, this is unique for the IFSC, because the momentum is reduced as the algebraic constraint
+#problem.add_equation(" -( dx(dx(w)) + dz(dz(w)) ) + dz(p) -(T-S*Ra_ratio)   =0")
+#problem.add_equation(" -( dx(dx(T)) + dz(dz(T)) ) + w                       =0")
+
+
+
+#flag.print_file()
+#print(', '.join("%s: %s, \n" % item for item in flag_attrs.items()))
+
+#Ra_ratio=1.1
 
 #merge time snapshot
 #set_paths = list(pathlib.Path("IFSC_2D_without_shear").glob("IFSC_2D_without_shear_s*.h5"))
--- dedalus_setup.py
+++ dedalus_setup.py
@@ -3,6 +3,7 @@ from dedalus import public as de
 
 
 class flag(object):
+    
     def __init__(self):
         self.Lx=np.pi
         self.Lz=np.pi
@@ -13,59 +14,92 @@ class flag(object):
         self.spectral_z='Fourier'
         self.name='test'
         self.flow='not_defined'
+        
+        self.Ra_ratio=1# the parameter for IFSC
+        self.ks=1# parameter for the large scale shear in IFSC with shear
+        self.F_sin=1# amplitude for the large scale shear in IFSC with shear
+        
         self.current_path='./'#This is the current folder path that might need to be specified is run on cluster
+    
     def print_screen(self):
         flag_attrs=vars(self)
         print(', '.join("%s: %s, \n" % item for item in flag_attrs.items()))
+    
     def print_file(self):
         flag_text=open(self.current_path+self.name+'/flag.txt','w+')
         flag_attrs=vars(self)
         print(', '.join("%s: %s, \n" % item for item in flag_attrs.items()),file=flag_text)
         flag_text.close()
+        
+    def build_domain(self):
+        if self.flow in ['IFSC_2D_without_shear', 'IFSC_2D_with_shear']:
+            x_basis = de.Fourier('x', flag.Nx, interval=(0,flag.Lx), dealias=3/2)
+            z_basis = de.Fourier('z', flag.Nz, interval=(0,flag.Lz), dealias=3/2)
+            domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)
+        return domain
 
-def build_domain(flag)
-    if flag.flow in ['IFSC_2D_without_shear', 'IFSC_2D_with_shear']
-
-
-def governing_equation(flag,domain):
-    if flag.flow == 'IFSC_2D_without_shear':
-        x_basis = de.Fourier('x', flag.Nx, interval=(0,flag.Lx), dealias=3/2)
-        z_basis = de.Fourier('z', flag.Nz, interval=(0,flag.Lz), dealias=3/2)
-        domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)
-        problem = de.IVP(domain,variables=['p','u','w','S','T'])
-        problem.parameters['Ra_ratio']=flag.Ra_ratio
-        problem.add_equation("p=0",condition="(nx==0) and (nz==0)")
-        problem.add_equation("u=0",condition="(nx==0) and (nz==0)")
-        problem.add_equation("dx(u)+dz(w)=0",condition="(nx!=0) or (nz!=0)")
-        problem.add_equation("dt(S) - (dx(dx(S)) + dz(dz(S))) +w =-u*dx(S)-w*dz(S) ")
-        problem.add_equation("- (dx(dx(u))+dz(dz(u)) ) +dx(p) = 0", condition="(nx!=0) or (nz!=0)")
-        problem.add_equation(" - ( dx(dx(w)) + dz(dz(w)) ) + dz(p) -(T-S*Ra_ratio)  =0")
-        problem.add_equation(" - ( dx(dx(T)) + dz(dz(T)) ) + w =0")
+    def governing_equation(self,domain):
+        if self.flow == 'IFSC_2D_without_shear':
+            problem = de.IVP(domain,variables=['p','u','w','S','T'])
+            problem.parameters['Ra_ratio']=flag.Ra_ratio
+            problem.add_equation("p=0",condition="(nx==0) and (nz==0)")
+            problem.add_equation("u=0",condition="(nx==0) and (nz==0)")
+            problem.add_equation("dx(u)+dz(w)=0",condition="(nx!=0) or (nz!=0)")
+            problem.add_equation("dt(S) - (dx(dx(S)) + dz(dz(S))) +w =-u*dx(S)-w*dz(S) ")
+            problem.add_equation("- (dx(dx(u))+dz(dz(u)) ) +dx(p) = 0", condition="(nx!=0) or (nz!=0)")
+            problem.add_equation(" - ( dx(dx(w)) + dz(dz(w)) ) + dz(p) -(T-S*Ra_ratio)  =0")
+            problem.add_equation(" - ( dx(dx(T)) + dz(dz(T)) ) + w =0")
         # This is assumed to be doubly periodic, no boundary conditions
-    elif flag.flow == "IFSC_2D_with_shear":
-        problem.add_equation("p=0",condition="(nx==0) and (nz==0)")
-        problem.add_equation("u=0",condition="(nx==0) and (nz==0)")
-        problem.add_equation("dx(u)+dz(w)=0",condition="(nx!=0) or (nz!=0)")
-        problem.add_equation("dt(S) - (dx(dx(S)) + dz(dz(S))) +w =-u*dx(S)-w*dz(S) ")
-        problem.add_equation("- (dx(dx(u))+dz(dz(u)) ) +dx(p) = F_sin*sin(ks*z)", condition="(nx!=0) or (nz!=0)")
-        problem.add_equation(" - ( dx(dx(w)) + dz(dz(w)) ) + dz(p) -(T-S*Ra_ratio)  =0")
-        problem.add_equation(" - ( dx(dx(T)) + dz(dz(T)) ) + w =0")
-        #this is triple periodic, no boundary conditions.
-    elif flag.flow == "channel":
-        problem.add_equation("dx(u) + dy(v)+wz=0")
-        problem.add_equation("dt(u) - 1/Re*(dx(dx(u)) + dy(dy(u)) + dz(uz) ) + dx(p) = -u*dx(u) - v*dy(u) - w*uz + 2/Re")
-        problem.add_equation("dt(v) - 1/Re*(dx(dx(v)) + dy(dy(v)) + dz(vz) ) + dy(p) = -u*dx(v) - v*dy(v) - w*vz")
-        problem.add_equation("dt(w) - 1/Re*(dx(dx(w)) + dy(dy(w)) + dz(wz) ) + dz(p) = -u*dx(w) v*dy(w) - w*wz")
-        problem.add_equation("uz - dz(u) =0")
-        problem.add_equation("vz - dz(v) =0")
-        problem.add_equation("wz - dz(w) =0")
-        problem.add_bc("left(u) = 0")
-        problem.add_bc("left(v) = 0")
-        problem.add_bc("left(w) = 0")
-        problem.add_bc("right(u) = 0")
-        problem.add_bc("right(v) = 0")
-        problem.add_bc("right(w) = 0", condition="(nx !=0) or (ny !=0)")
-        problem.add_bc("right(p) = 0", condition="(nx == 0) and (ny == 0)")
-    elif flag.flow=='not_defined':
-        raise TypeError('flag.flow is not defined yet') 
-    return problem
+        elif self.flow == "IFSC_2D_with_shear":
+            problem.add_equation("p=0",condition="(nx==0) and (nz==0)")
+            problem.add_equation("u=0",condition="(nx==0) and (nz==0)")
+            problem.add_equation("dx(u)+dz(w)=0",condition="(nx!=0) or (nz!=0)")
+            problem.add_equation("dt(S) - (dx(dx(S)) + dz(dz(S))) +w =-u*dx(S)-w*dz(S) ")
+            problem.add_equation("- (dx(dx(u))+dz(dz(u)) ) +dx(p) = F_sin*sin(ks*z)", condition="(nx!=0) or (nz!=0)")
+            problem.add_equation(" - ( dx(dx(w)) + dz(dz(w)) ) + dz(p) -(T-S*Ra_ratio)  =0")
+            problem.add_equation(" - ( dx(dx(T)) + dz(dz(T)) ) + w =0")
+            #this is triple periodic, no boundary conditions.
+        elif self.flow == "channel":
+            problem.add_equation("dx(u) + dy(v)+wz=0")
+            problem.add_equation("dt(u) - 1/Re*(dx(dx(u)) + dy(dy(u)) + dz(uz) ) + dx(p) = -u*dx(u) - v*dy(u) - w*uz + 2/Re")
+            problem.add_equation("dt(v) - 1/Re*(dx(dx(v)) + dy(dy(v)) + dz(vz) ) + dy(p) = -u*dx(v) - v*dy(v) - w*vz")
+            problem.add_equation("dt(w) - 1/Re*(dx(dx(w)) + dy(dy(w)) + dz(wz) ) + dz(p) = -u*dx(w) v*dy(w) - w*wz")
+            problem.add_equation("uz - dz(u) =0")
+            problem.add_equation("vz - dz(v) =0")
+            problem.add_equation("wz - dz(w) =0")
+            problem.add_bc("left(u) = 0")
+            problem.add_bc("left(v) = 0")
+            problem.add_bc("left(w) = 0")
+            problem.add_bc("right(u) = 0")
+            problem.add_bc("right(v) = 0")
+            problem.add_bc("right(w) = 0", condition="(nx !=0) or (ny !=0)")
+            problem.add_bc("right(p) = 0", condition="(nx == 0) and (ny == 0)")
+        elif self.flow=='not_defined':
+            raise TypeError('flag.flow is not defined yet') 
+        return problem
+
+    def initial_condition(self,domain,solver):
+        if self.flow in ['IFSC_2D_without_shear', 'IFSC_2D_with_shear']:
+    
+            x = domain.grid(0)
+            z = domain.grid(1)
+            u = solver.state['u']
+            w = solver.state['w']
+            S = solver.state['S']
+            p = solver.state['p']
+            T = solver.state['T']
+            
+            gshape = domain.dist.grid_layout.global_shape(scales=1)
+            slices = domain.dist.grid_layout.slices(scales=1)
+            rand = np.random.RandomState(seed=23)
+            noise = rand.standard_normal(gshape)[slices]
+            
+            k_opt=(1/2*(-2-self.Ra_ratio+np.sqrt(self.Ra_ratio**2+8*self.Ra_ratio)))**(1/4)
+            w['g'] = self.A_elevator*np.sin(k_opt*x) + self.A_noise*noise
+            u['g'] = self.A_noise*noise + self.A_shear*self.F_sin/self.ks**2*np.sin(self.ks*z)
+            S['g'] = -1/self.Ra_ratio*(k_opt**2+1/k_opt**2)*self.A_elevator*np.sin(k_opt*x) + self.A_noise*noise
+            T['g'] = -1/(k_opt**2)*self.A_elevator*np.sin(k_opt*x) + self.A_noise*noise
+            p['g'] = self.A_noise*noise
+
+
+
