3e68185727eb0b6943280512d597f622d47e112b
cliu124 <39631690+cliu124@users.noreply.github.com>
2021-08-31 14:01:27 -0700

    test

--- dedalus_setup.py
+++ dedalus_setup.py
@@ -33,8 +33,8 @@ class flag(object):
     def print_file(self):
         flag_text=open(self.current_path+self.name+'/flag.txt','w+')
         flag_attrs=vars(self)
-        print(', Attributes: Value,\n,',file=flag_text)
-        print(', '.join("%s: %s, \n" % item for item in flag_attrs.items()),file=flag_text)
+        print(', Attributes: Value,\n',file=flag_text)
+        print(','+', '.join("%s: %s, \n" % item for item in flag_attrs.items()),file=flag_text)
         flag_text.close()
         
     def build_domain(self):
