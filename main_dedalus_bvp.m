eb2d8757af250a198c33e013f6e20d13fd3f27a8
cliu124 <39631690+cliu124@users.noreply.github.com>
2021-12-05 15:21:24 -0800

    Update 2021 shear

--- main_dedalus_bvp.m
+++ main_dedalus_bvp.m
@@ -37,7 +37,7 @@ close all;
 %            '12646320',
 %            '12646324'}; %           '12646314',
 
-slurm_num={'12669653'}; 
+slurm_num={'12676035'}; 
 %'12640113', %%This case is wierd... not right
     
 for slurm_ind=1:length(slurm_num)
