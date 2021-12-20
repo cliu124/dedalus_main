cec0f5ece8d40af90f1ec384ff4d13256a886274
cliu124 <39631690+cliu124@users.noreply.github.com>
2021-12-19 18:16:47 -0800

    Update eigenvalue, not use lambda

--- dedalus_setup.py
+++ dedalus_setup.py
@@ -1256,7 +1256,7 @@ class flag(object):
                 problem = de.EVP(domain, variables=\
                     ['u_tilde','d_u_tilde','v_tilde','d_v_tilde', \
                     'w_hat','p_hat','T_hat','d_T_hat', \
-                    'S_hat','d_S_hat'],eigenvalue='lambda')
+                    'S_hat','d_S_hat'],eigenvalue='eig_val')
             
                 
             problem.parameters['Pr'] = self.Pr 
@@ -1360,12 +1360,12 @@ class flag(object):
                 
                 #coupling between real and imag due to shear
                 if self.F_sin=='z':
-                    problem.add_equation('dz(d_T_hat)-w_hat*dy_T_mean-(kx*kx+ky*ky)*T_hat-1j*Pe_T*kx*(z-1/2)*T_hat-lambda*T_hat=0')
-                    problem.add_equation('dz(d_S_hat)-1/tau*w_hat*dy_S_mean-(kx*kx+ky*ky)*S_hat-1j*Pe_S/tau*kx*(z-1/2)*S_hat-lambda*S_hat=0')   
+                    problem.add_equation('dz(d_T_hat)-w_hat*dy_T_mean-(kx*kx+ky*ky)*T_hat-1j*Pe_T*kx*(z-1/2)*T_hat-eig_val*T_hat=0')
+                    problem.add_equation('dz(d_S_hat)-1/tau*w_hat*dy_S_mean-(kx*kx+ky*ky)*S_hat-1j*Pe_S/tau*kx*(z-1/2)*S_hat-eig_val*S_hat=0')   
                 else:
                     print(self.F_sin)
-                    problem.add_equation('dz(d_T_hat)-w_hat*dy_T_mean-(kx*kx+ky*ky)*T_hat-1j*Pe_T*kx*F_sin*sin(ks*z)*T_hat-lambda*T_hat=0')
-                    problem.add_equation('dz(d_S_hat)-1/tau*w_hat*dy_S_mean-(kx*kx+ky*ky)*S_hat-1j*Pe_S/tau*kx*F_sin*sin(ks*z)*S_hat-lambda*S_hat=0')   
+                    problem.add_equation('dz(d_T_hat)-w_hat*dy_T_mean-(kx*kx+ky*ky)*T_hat-1j*Pe_T*kx*F_sin*sin(ks*z)*T_hat-eig_val*T_hat=0')
+                    problem.add_equation('dz(d_S_hat)-1/tau*w_hat*dy_S_mean-(kx*kx+ky*ky)*S_hat-1j*Pe_S/tau*kx*F_sin*sin(ks*z)*S_hat-eig_val*S_hat=0')   
               
             
             if self.z_bc_w_left=='periodic' and self.z_bc_w_right=='periodic':
