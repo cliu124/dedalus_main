477e88ceb617540fb678667751734a24e52709ca
cliu124 <39631690+cliu124@users.noreply.github.com>
2022-07-17 22:48:01 -0700

    Update 2022/07/17
    add the time step for storing

--- dedalus_main.py
+++ dedalus_main.py
@@ -714,6 +714,9 @@ elif flag.flow == 'double_diffusive_shear_2D':
     elif flag.flow_sub_double_diffusive_shear_2D=='primitive_periodic_salt_finger':
         flag.post_store_dt=1
         flag.stop_sim_time=5
+    elif flag.flow_sub_double_diffusive_shear_2D=='primitive_periodic_RBC':
+        flag.post_store_dt=1
+        flag.stop_sim_time=5
 else:
     print('1')
     #flag.post_store_dt=0.000001/flag.Ra_T;
