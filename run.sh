08f00c2bc4a930190a26158b299df6267d95f9f4
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-03-07 11:09:29 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 conda activate dedalus2
+now=$(date+"%Y%m%d%H%M%S")
+WORKDIR= dedalus_$now
+mkdir -p "$WORKDIR" && cp -r dedalus*.py "$WORKDIR" && cp -r dedalus.cfg "$WORKDIR" && cp submit_dedalus "$WORKDIR" && cd "$WORKDIR" || exit -1
 mpiexec -n 1 python3 dedalus_main.py
-cp -r analysis /mnt/d/Data/dedalus/local
+cd ..
+cp -r "$WORKDIR" /mnt/d/Data/dedalus
