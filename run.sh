f95a4f777b14333763cc910399f78fc2f1892109
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-03-07 11:18:24 -0800

    get rid

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 conda activate dedalus2
 now=$(date +"%Y%m%d%H%M%S")
 WORKDIR= dedalus_$now
-mkdir -p "$WORKDIR"
-cp -r dedalus*.py "$WORKDIR"
-cp -r dedalus.cfg "$WORKDIR" 
-cd "$WORKDIR"
+mkdir $WORKDIR
+cp -r dedalus*.py $WORKDIR
+cp -r dedalus.cfg $WORKDIR 
+cd $WORKDIR
 mpiexec -n 1 python3 dedalus_main.py
 cd ..
 cp -r "$WORKDIR" /mnt/d/Data/dedalus
