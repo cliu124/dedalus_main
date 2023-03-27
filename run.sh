4e840683988cb7f6e9cc4eefe14a7c52cccdab59
cliu124 <39631690+cliu124@users.noreply.github.com>
2023-03-26 17:11:48 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 conda activate dedalus2
 now="dedalus_$(date +"%Y%m%d%H%M%S")"
-ln -s /dedalus_20230326162736/analysis/analysis_s1.h5 restart.h5
+ln -s /home/cliu124/dedalus_main/dedalus_20230326162736/analysis/analysis_s1.h5 restart.h5
 mpiexec -n 1 python3 dedalus_main.py
 echo $now
 mkdir -p "$now"
